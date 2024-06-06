da993303e0024a0f7fbf25dc502001cd4b92173a
Juan Ignacio Colli <nachocolli1@gmail.com>
2024-06-06 11:04:27 -0300

    Add baiml Products page

---
+++ src/app/products/baiml/page.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function baimlProductsPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await axios.get("/api/products");
+      setProducts(res.data.products);
+      console.log(res.data.products);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      {products.map((prod) => {
+        return (
+          <div key={prod.productId}>
+            <h4>{prod.name}</h4>
+            <p>{prod.description}</p>
+            <p>{prod.category}</p>
+            <p>{prod.unit}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default baimlProductsPage;
