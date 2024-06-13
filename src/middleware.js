e1f21898757308f77c98356d51f440544f12882a
Juan Ignacio Colli <nachocolli1@gmail.com>
2024-06-13 10:57:33 -0300

    Debug Middleware #15 adding cookieName to getToken()

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,14 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  const token = await getToken({ req });
+  const token = await getToken({
+    req,
+    cookieName:
+      process.env.NODE_ENV === "development"
+        ? "next-auth.session-token"
+        : "__Secure-next-auth.session-token",
+    secret: process.env.NEXTAUTH_SECRET,
+  });
   console.log({ token: token });
   const url = req.nextUrl.clone();
   console.log({ url: url });
@@ -15,16 +22,16 @@ export async function middleware(req) {
     url.searchParams.set("error", "Primero debes iniciar sesion");
     url.searchParams.set("callbackUrl", destination);
 
-    try { 
-      const fullUrl = url.toString(); 
-      console.log({ fullUrl: fullUrl }); 
- 
-      return NextResponse.redirect(fullUrl); 
-    } catch (error) { 
-      console.error("Error constructing redirect URL:", error); 
-      return NextResponse.error(); 
-    } 
-  } 
+    try {
+      const fullUrl = url.toString();
+      console.log({ fullUrl: fullUrl });
+
+      return NextResponse.redirect(fullUrl);
+    } catch (error) {
+      console.error("Error constructing redirect URL:", error);
+      return NextResponse.error();
+    }
+  }
 
   if (req.nextUrl.pathname.startsWith("/admin")) {
     if (token.user.rol !== "admin") {
