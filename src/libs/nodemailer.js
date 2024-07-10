2158ef4809d36e840fa7fca613b50c03ddf4e490
Alondra Tonelli <nachocolli1@gmail.com>
2024-07-10 09:11:51 -0300

    Change, nodemailer & mail to oauth2.0

--- src/libs/nodemailer.js
+++ src/libs/nodemailer.js
@@ -1,11 +1,31 @@
 import nodemailer from 'nodemailer';
+import { google } from 'googleapis';
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
-  }
-});
+const CLIENT_ID = process.env.GMAIL_CLIENT_ID;
+const CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET;
+const REDIRECT_URI = "http://localhost:3000"
+const REFRESH_TOKEN = process.env.GMAIL_REFRESH_TOKEN;
+console.log({CLIENT_ID,CLIENT_SECRET,REDIRECT_URI,REFRESH_TOKEN,Email:process.env.EMAIL_USER});
 
-export default transporter;
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+async function createTransporter() {
+  const accessToken = await oAuth2Client.getAccessToken();
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.EMAIL_USER,
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      refreshToken: REFRESH_TOKEN,
+      accessToken: accessToken.token,
+    },
+  });
+
+  return transporter;
+}
+
+export default createTransporter;
--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,7 +1,9 @@
-import transporter from "@/libs/nodemailer";
+import createTransporter from "@/libs/nodemailer";
 
 async function sendEmail({ to, subject, text }) {
   try {
+    const transporter = await createTransporter();
+
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to,
@@ -10,6 +12,7 @@ async function sendEmail({ to, subject, text }) {
     };
 
     const result = await transporter.sendMail(mailOptions);
+    console.log(`Email sent ${result}`);
     return result;
   } catch (error) {
     console.error('Error al enviar el email:', error);
