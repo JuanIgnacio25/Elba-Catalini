e6530324027532f8e2ffe33cde4966d0cbfe87fb
JuanIgnacio25 <nachocolli1@gmail.com>
2024-08-01 18:26:53 -0300

    Add, method to generate recovery token

--- src/utils/token/generateRecoveryToken.js
+++ src/utils/token/generateRecoveryToken.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
 
-const generateVerificationToken = (email) => {
-  const payload = { email };
+const generateRecoveryToken = (userId) => {
+  const payload = {userId};
   const secret = process.env.JWT_SECRET;
   const options = { expiresIn: "1h" };
 
   return jwt.sign(payload, secret, options);
 };
 
-export default generateVerificationToken;
+export default generateRecoveryToken;
