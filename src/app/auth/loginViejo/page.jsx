1dc952b3b343beb63e8c2348f8874cdcc840e7db
JuanIgnacio25 <nachocolli1@gmail.com>
2024-08-30 10:17:29 -0300

    Fix,import in LoginViejo

--- src/app/auth/loginViejo/page.jsx
+++ src/app/auth/loginViejo/page.jsx
@@ -5,7 +5,7 @@ import LoginViejo from "@/components/views/auth/Login/LoginViejo";
 function LoginViejoPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Login/>
+      <LoginViejo/>
     </Suspense>
   );
 }
