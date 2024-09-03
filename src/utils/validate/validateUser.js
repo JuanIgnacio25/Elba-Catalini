eecb0f20d782940fbc3d270ef41fdf898e684a2f
JuanIgnacio25 <nachocolli1@gmail.com>
2024-09-03 17:06:10 -0300

    Modify, adding new validations fields

--- src/utils/validate/validateUser.js
+++ src/utils/validate/validateUser.js
@@ -31,11 +31,59 @@ addErrors(ajv);
 
 const signupDtoSchema = Type.Object(
   {
-    fullname: Type.String({
+    companyName: Type.String({
       minLength: 2,
       errorMessage: {
-        type: "El fullname debe ser un String",
-        minLength: "El fullname debe tener más de un carácter",
+        type: "companyName debe ser un String",
+        minLength: "La razon social debe tener más de un carácter",
+      },
+    }),
+    cuit: Type.String({
+      pattern: "^[0-9]+$",
+      minLength:10,
+      maxLength:11,
+      errorMessage: {
+        type: "El CUIT debe ser un String",
+        pattern: "El CUIT solo debe contener números",
+        minLength: "El CUIT debe tener entre 10 y 11 caracteres",
+        maxLength: "El CUIT debe tener entre 10 y 11 caracteres",
+      },
+    }),
+    phoneNumber: Type.String({
+      pattern: "^[0-9]+$",
+      minLength:8,
+      errorMessage: {
+        type: "El Numero de Celular debe ser un String",
+        pattern: "El Numero de Celular solo debe contener números",
+        minLength: "El Numero de Celular debe tener mas de 8 caracteres",
+      },
+    }),
+    purchasingManagerName: Type.String({
+      minLength:2,
+      errorMessage: {
+        type: "El nombre del encargado de compras debe ser un String",
+        minLength: "El nombre del encargado de compras debe tener mas de 2 caracteres",
+      },
+    }),
+    location: Type.String({
+      minLength:2,
+      errorMessage: {
+        type: "La localidad y provincia debe ser un String",
+        minLength: "La localidad y provincia debe tener mas de 2 caracteres",
+      },
+    }),
+    address: Type.String({
+      minLength:2,
+      errorMessage: {
+        type: "La direccion debe ser un String",
+        minLength: "La direccion debe tener mas de 2 caracteres",
+      },
+    }),
+    carrier: Type.String({
+      minLength:2,
+      errorMessage: {
+        type: "El transporte debe ser un String",
+        minLength: "El transporte debe tener mas de 2 caracteres",
       },
     }),
     email: Type.String({
