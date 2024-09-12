21b8a9bb55becbfaddf0a4656a721e4a185338b9
Juan Ignacio Colli <nachocolli1@gmail.com>
2024-09-12 11:03:30 -0300

    Add, PasswordRecovery , view , funtionalities , responsive styles

--- src/components/views/auth/PasswordRecovery/PasswordRecovery.jsx
+++ src/components/views/auth/PasswordRecovery/PasswordRecovery.jsx
@@ -1,9 +1,16 @@
-import "./passwordRecovery.css"
+import "./passwordRecovery.css";
+import PasswordRecoveryMain from "@/components/views/auth/PasswordRecovery/PasswordRecoveryMain";
 
 function PasswordRecovery() {
   return (
-    <div>PasswordRecovery</div>
-  )
+    <div className="recovery-token-container">
+      <div className="recovery-token">
+        <div className="recovery-token-side recovery-token-side-left"></div>
+        <PasswordRecoveryMain />
+        <div className="recovery-token-side recovery-token-side-right"></div>
+      </div>
+    </div>
+  );
 }
 
-export default PasswordRecovery
+export default PasswordRecovery;
