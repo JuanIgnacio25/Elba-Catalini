21edbb33e22261680dc2334288aff1d39ecafa8f
Juan Ignacio Colli <nachocolli1@gmail.com>
2024-11-20 15:11:10 -0300

    Add, Dropdown view , styles & funtionalities

---
+++ src/components/common/Dropdown/Dropdown.jsx
@@ -0,0 +1,41 @@
+"use client";
+
+import "./dropdown.css";
+
+import { useState } from "react";
+import Link from "next/link";
+
+import { IoIosArrowForward } from "react-icons/io";
+
+
+function Dropdown({ category, options, baseUrl }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      className="dropdown"
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
+    >
+      <Link href={`${baseUrl}/${category.slug}`} className="dropdown-category">
+        {category.name}
+      </Link>
+      {isOpen && (
+        <div className="dropdown-menu">
+          {options.map((option) => (
+            <Link
+              key={option.slug}
+              href={`${baseUrl}/${category.slug}/${option.slug}`}
+              className="dropdown-item"
+            > 
+              <IoIosArrowForward className="dropdown-icon"/>
+              {option.name}
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Dropdown;
