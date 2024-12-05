bb82942f94cadd8de1d28899a12eeaa612da8fc3
Juan Ignacio Colli <nachocolli1@gmail.com>
2024-12-05 14:43:47 -0300

    Modified, Fallback in Suspense

--- src/components/views/Products/ToxicShine/ToxicShine.jsx
+++ src/components/views/Products/ToxicShine/ToxicShine.jsx
@@ -1,19 +1,29 @@
-import "@/components/views/Products/ToxicShine/toxicShine.css"
+import "@/components/views/Products/ToxicShine/toxicShine.css";
 
-import PathHeader from "@/components/common/PathHeader/PathHeader"
-import ToxicShineMain from "@/components/views/Products/ToxicShine/ToxicShineMain"
+import PathHeader from "@/components/common/PathHeader/PathHeader";
+import ToxicShineMain from "@/components/views/Products/ToxicShine/ToxicShineMain";
+import ProductsMainFallback from "@/components/Fallbacks/ProductsMainFallback/ProductsMainFallback";
 
-import { Suspense } from "react"
+import { TOXIC_SHINE_CATEGORIES } from "@/constants/categories";
+
+import { Suspense } from "react";
 
 function ToxicShine() {
   return (
     <div className="toxic-products-container">
-      <PathHeader/>
-      <Suspense fallback={<div>..Loading</div>}>
-        <ToxicShineMain/>
+      <PathHeader />
+      <Suspense
+        fallback={
+          <ProductsMainFallback
+            categories={TOXIC_SHINE_CATEGORIES}
+            enabled={false}
+          />
+        }
+      >
+        <ToxicShineMain />
       </Suspense>
     </div>
-  )
+  );
 }
 
-export default ToxicShine
+export default ToxicShine;
