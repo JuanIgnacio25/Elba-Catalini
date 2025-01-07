d817541a900b380664d94b2f3a4cde106933b134
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-01-07 14:15:39 -0300

    Modified, now go to top button appears when the user scrolls down 300px

--- src/components/FixedActions/FixedActions.jsx
+++ src/components/FixedActions/FixedActions.jsx
@@ -1,27 +1,50 @@
-"use client"
+"use client";
 
+import { useState, useEffect } from "react";
 import { IoMdArrowUp } from "react-icons/io";
 
 export default function FixedActions() {
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowScrollButton(true);
+      } else {
+        setShowScrollButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="fixed bottom-5 right-5 flex flex-col gap-4 z-50">
+    <div className="fixed bottom-5 right-5 flex flex-col items-center gap-4 z-50">
+      <div className="w-11 h-11">
+        {showScrollButton && (
+          <button
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            className="w-11 h-11 bg-gray-500 text-white rounded-full flex items-center justify-center shadow-lg hover:bg-red-500 hover:text-gray-600 hover:scale-110 transition-all"
+            aria-label="Volver arriba"
+          >
+            <IoMdArrowUp className="w-8 h-8" />
+          </button>
+        )}
+      </div>
+
       <a
         href="https://wa.me/5493471589042"
         target="_blank"
         rel="noopener noreferrer"
-        className=" w-11 h-11 rounded-full flex items-center justify-center shadow-lg hover:scale-110 transition-transform"
+        className="w-11 h-11 rounded-full flex items-center justify-center shadow-lg hover:scale-110 transition-transform"
         aria-label="WhatsApp"
       >
         <img src="/whatsapp-logo.png" alt="WhatsApp" className="w-11 h-11" />
       </a>
-
-      <button
-        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-        className="w-11 h-11 bg-gray-500 text-white rounded-full flex items-center justify-center shadow-lg hover:bg-red-500 hover:text-gray-600 hover:scale-110 transition-all"
-        aria-label="Volver arriba"
-      >
-        <IoMdArrowUp className="w-8 h-8"/>
-      </button>
     </div>
   );
 }
