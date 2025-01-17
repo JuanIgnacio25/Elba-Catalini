71b84b1cd2af20723e69154e1a5e5974325ba82c
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-01-17 17:05:59 -0300

    Modified, validateQuantity when click order continue

--- src/components/views/Cart/CartCards.jsx
+++ src/components/views/Cart/CartCards.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useCart } from "@/context/CartContext";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 import CartCard from "@/components/views/Cart/CartCard";
@@ -9,14 +10,32 @@ import CartCloseOrder from "@/components/views/Cart/CartCloseOrder";
 import ConfirmModal from "@/components/common/ConfirmModal/ConfirmModal";
 import FallbackSpinner from "@/components/common/FallbackSpinner/FallbackSpinner";
 
-function CartCards() {
+import validateQuantity from "@/utils/validate/validateQuantity";
 
-  const { cart, loading, clearTheCart} = useCart();
+import { MdError } from "react-icons/md";
+
+function CartCards() {
+  const router = useRouter();
+
+  const { cart, loading, clearTheCart } = useCart();
   const [isClearTheCartModalOpen, setIsClearTheCartModalOpen] = useState(false);
+  const [error, setError] = useState(null);
 
   const openClearTheCartModal = () => setIsClearTheCartModalOpen(true);
   const closeClearTheCartModal = () => setIsClearTheCartModalOpen(false);
 
+  const handleCloseOrder = () => {
+    const validateProductsQuantity = cart.products.every((prod) =>
+      validateQuantity(prod.quantity)
+    );
+    if (!validateProductsQuantity) {
+      setError("La cantidad de los productos debe ser mayor a 0");
+      window.scrollTo({top: 0, behavior: "smooth"});
+      return;
+    }
+    setError(null);
+    router.push("/cart/confirmOrder");
+  };
 
   if (loading) {
     return (
@@ -63,6 +82,11 @@ function CartCards() {
     <div className="cart-main">
       <div className="cart-main-title">
         <h1>Detalles del Pedido</h1>
+        {error && (
+          <div className="flex flex-row items-center gap-1 text-xs md:text-sm lg:text-base text-red-500">
+            <MdError /> <p>{error}</p>
+          </div>
+        )}
       </div>
       <div className="cart-cards-container">
         <table className="cart-cards">
@@ -89,7 +113,7 @@ function CartCards() {
           </tbody>
         </table>
       </div>
-      <CartCloseOrder/>
+      <CartCloseOrder handleCloseOrder={handleCloseOrder} />
 
       <ConfirmModal
         isOpen={isClearTheCartModalOpen}
--- src/components/views/Cart/CartCloseOrder.jsx
+++ src/components/views/Cart/CartCloseOrder.jsx
@@ -1,15 +1,11 @@
 "use client"
 
-import { useRouter } from "next/navigation";
-
-function CartCloseOrder() {
-
-  const router = useRouter();
+function CartCloseOrder({handleCloseOrder}) {
 
   return (
     <div className="cart-close-container">
       <div className="cart-close">
-        <button className="cart-close-button" onClick={() => router.push("/cart/confirmOrder")}>Continuar pedido</button>
+        <button className="cart-close-button" onClick={handleCloseOrder}>Continuar pedido</button>
       </div>
     </div>
   );
