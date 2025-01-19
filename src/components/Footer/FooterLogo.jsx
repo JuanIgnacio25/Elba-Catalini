539394ddc86c63601a9730d7ea1fde9f685230f6
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-01-19 19:19:55 -0300

    Add, responsive styles to Footer

--- src/components/Footer/FooterLogo.jsx
+++ src/components/Footer/FooterLogo.jsx
@@ -3,10 +3,18 @@ import Image from "next/image";
 function FooterLogo() {
   return (
     <div className="footer-main-logo">
-          <div className="footer-main-logo-container">
-          </div>
-        </div>
-  )
+      <div className="footer-main-logo-container">
+        <Image
+          src="/logo-lca-ps.png"
+          alt="Logo-Main"
+          width={500}
+          height={500}
+          className="footer-main-image"
+          priority
+        />
+      </div>
+    </div>
+  );
 }
 
-export default FooterLogo
+export default FooterLogo;
