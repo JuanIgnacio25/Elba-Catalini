4eecbcd68c5c1687c3fb73ea2d5435ee2bcd03d7
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-01-24 17:35:38 -0300

    Add, ProductsByCategory view , responsive styles

---
+++ src/app/products/category/[kind]/[category]/page.jsx
@@ -0,0 +1,9 @@
+import ProductsByCategory from "@/components/views/Products/ProductsByCategory/ProductsByCategory"
+
+function productCategoryPage() {
+  return (
+    <ProductsByCategory/>
+  )
+}
+
+export default productCategoryPage
