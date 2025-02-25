4052e2440a73f98f73adc7a6f9c6c69f409753df
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-02-25 11:12:43 -0300

    Modified, footer height

--- src/components/Footer/FooterInfo.jsx
+++ src/components/Footer/FooterInfo.jsx
@@ -3,7 +3,7 @@ function FooterInfo() {
   return (
     <div className="footer-main-info">
           <ul>
-            <li><span>Direccion:</span> Bv.Centenario 639 - (2500) Cañada de Gomez</li>
+            <li><span>Direccion:</span> Bv.Centenario 639 - Cañada de Gomez</li>
             <li><span>Cel:</span> +3471 589042</li>
             <li><span>Email:</span> ventascatalini@gmail.com</li>
             <li><span>Horarios:</span> Lunes a Viernes 8 a 12 - 15 a 19</li>
