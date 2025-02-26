f7356d51cbab8062b1421464468ee738b3685882
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-02-26 16:50:34 -0300

    Modified, responsive min-height

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,15 +1,17 @@
+import "./not-found.css"
+
 function notFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-96 bg-gray-100 px-4">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+    <div className="not-found-container flex flex-col items-center justify-center min-h-96 bg-gray-100 px-4">
+      <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-gray-800 text-center mb-4">
         Página no encontrada
       </h1>
-      <p className="text-lg text-gray-600">
+      <p className="text-base lg:text-lg text-gray-600 text-center">
         El enlace no corresponde a ninguna página válida.
       </p>
       <a
         href="/"
-        className="mt-5 px-5 py-2 bg-red-600 text-white font-medium text-base rounded-lg shadow-md hover:bg-red-700 transition"
+        className="mt-4 px-4 py-1 md:mt-5 md:px-5 md:py-2 bg-red-600 text-white font-medium text-sm sm:text-base rounded-lg shadow-md hover:bg-red-700 transition"
       >
         Volver al inicio
       </a>
