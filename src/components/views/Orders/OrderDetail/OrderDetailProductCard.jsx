e2a9bfa0f88024ce05eabe901b3bf4b34badebea
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-02-26 16:08:26 -0300

    Modified, min-height

--- src/components/views/Orders/OrderDetail/OrderDetailProductCard.jsx
+++ src/components/views/Orders/OrderDetail/OrderDetailProductCard.jsx
@@ -9,7 +9,7 @@ function OrderDetailProductCard({ product }) {
           <Link href="/" className="order-detail-product-card-img-link">
             <Image
               className="order-detail-product-card-img"
-              src={product.images[0]}
+              src={product.images[0].url}
               alt="Logo-Product"
               width={485}
               height={485}
