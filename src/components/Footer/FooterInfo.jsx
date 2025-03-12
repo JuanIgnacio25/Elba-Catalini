3d998347a5e3b89a3d40ad9004bde02757cee025
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-03-12 17:06:03 -0300

    Modified, saturday schedule

--- src/components/Footer/FooterInfo.jsx
+++ src/components/Footer/FooterInfo.jsx
@@ -6,7 +6,7 @@ function FooterInfo() {
             <li><span>Direccion:</span> Bv.Centenario 639 - Cañada de Gomez</li>
             <li><span>Cel:</span> +3471 589042</li>
             <li><span>Email:</span> ventascatalini@gmail.com</li>
-            <li><span>Horarios:</span> Lunes a Viernes 8 a 12 - 15 a 19</li>
+            <li><span>Horarios:</span> Lunes a Viernes 8 a 12 - 15 a 19 - Sabados 8 a 12</li>
           </ul>
         </div>
   )
