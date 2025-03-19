614b9fab1f5e5000450f1c618893779c773cecc5
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-03-19 14:07:46 -0300

    Modified, Bandas Reflectivas dont show 'mts' in his unit anymore

--- src/utils/formatStoreProductUnit.js
+++ src/utils/formatStoreProductUnit.js
@@ -1,6 +1,6 @@
 const formatStoreProductUnit = (subCategory, unit) => {
 
-  const categories = ["Cable TPR","Caño Corrugado Abierto","Tubo Termocontraible","Cinta Helicoidal","Spaghetti PVC","Bandas Reflectivas"];
+  const categories = ["Cable TPR","Caño Corrugado Abierto","Tubo Termocontraible","Cinta Helicoidal","Spaghetti PVC"];
 
   if(categories.includes(subCategory)){
     return `${unit} mts`;
