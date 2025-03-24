a8f332d7dfb5cfcbfc41539db9f57887766e1cf6
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-03-24 20:14:49 -0300

    Modified, use Supense

--- src/components/views/admin/Dashboard.jsx
+++ src/components/views/admin/Dashboard.jsx
@@ -1,15 +1,21 @@
+import { Suspense } from "react";
+
+import AdminProductTableFallback from "@/components/Fallbacks/AdminProductTableFallback";
+
 import DashboardProductForm from "@/components/views/admin/DashboardProductForm";
 import DashboardProductsTable from "@/components/views/admin/DashboardProductsTable";
-/* import DashboardUsersTable from "@/components/views/admin/DashboardUsersTable"; */
+import DashboardUsersTable from "@/components/views/admin/DashboardUsersTable";
 
 function Dashboard() {
   return (
     <div className="w-full min-h-screen flex flex-col items-center my-8">
-      <DashboardProductsTable/>
-      <DashboardProductForm/>
-      {/* <DashboardUsersTable/> */}
+      <Suspense fallback={<AdminProductTableFallback />}>
+        <DashboardProductsTable />
+      </Suspense>
+      <DashboardProductForm />
+      <DashboardUsersTable />
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
