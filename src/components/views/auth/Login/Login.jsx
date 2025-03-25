158d1651905a8b34fe0c3fbdd2a598bd4cb4da54
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-03-25 18:57:34 -0300

    Modified, migrate Login component styles from CSS to Tailwind

--- src/components/views/auth/Login/Login.jsx
+++ src/components/views/auth/Login/Login.jsx
@@ -1,18 +1,16 @@
 import LoginMain from "@/components/views/auth/Login/LoginMain";
 import LoginMainFallback from "@/components/Fallbacks/LoginMainFallback";
-import "./login.css";
-
 import { Suspense } from "react";
 
 function Login() {
   return (
-    <div className="login-container">
-      <div className="login">
-        <div className="login-side login-side-left"></div>
+    <div className="flex flex-col justify-center items-center h-[47vh] sm:h-[50vh] lg:h-[60vh] my-6">
+      <div className="flex flex-row flex-wrap justify-start items-center w-full h-full max-w-[95vw] sm:max-w-[85vw] md:max-w-[75vw] lg:max-w-[52vw] bg-white shadow-lg rounded-lg overflow-hidden">
+        <div className="w-[8%] h-full bg-gradient-to-b from-red-500 to-black"></div>
         <Suspense fallback={<LoginMainFallback />}>
           <LoginMain />
         </Suspense>
-        <div className="login-side login-side-right"></div>
+        <div className="w-[8%] h-full bg-gradient-to-b from-red-500 to-black"></div>
       </div>
     </div>
   );
--- src/components/views/auth/Login/LoginMain.jsx
+++ src/components/views/auth/Login/LoginMain.jsx
@@ -2,9 +2,10 @@
 
 import { useState, useEffect } from "react";
 import { signIn } from "next-auth/react";
-import { useRouter, useSearchParams} from "next/navigation";
-import { useSession} from "next-auth/react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { ImSpinner8 } from "react-icons/im";
 
 function LoginMain() {
   const router = useRouter();
@@ -13,6 +14,7 @@ function LoginMain() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   useEffect(() => {
@@ -27,7 +29,6 @@ function LoginMain() {
   useEffect(() => {
     if (status === "authenticated") {
       let callbackUrl = searchParams.get("callbackUrl") || "/";
-
       if (callbackUrl.startsWith("/cart/")) {
         callbackUrl = `/cart/${session.user.cartId}`;
       }
@@ -37,58 +38,69 @@ function LoginMain() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     setError("");
 
-    const nextAuthResponse = await signIn("credentials", {
-      email: email,
-      password: password,
-      redirect: false,
-    });
-
-    if (!nextAuthResponse.ok) return setError(nextAuthResponse.error);
+    try {
+      const nextAuthResponse = await signIn("credentials", {
+        email: email,
+        password: password,
+        redirect: false,
+      });
+      if (!nextAuthResponse.ok) throw nextAuthResponse.error;
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <div className="login-main-container">
-      <h1 className="login-main-title">Iniciar Sesion</h1>
-      <form className="login-main-data-container" onSubmit={handleSubmit}>
-        <div className="login-main-data">
-          <p>Correo Electronico</p>
+    <div className="flex flex-col items-center justify-center w-[84%] ">
+      <h1 className="w-[80%] text-center pb-2 text-xl md:text-2xl font-bold text-gray-800 border-b">Ingresá a tu cuenta</h1>
+      <form className="w-full max-w-md bg-white p-6 rounded-lg" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-sm font-bold text-gray-700">Correo Electrónico</label>
           <input
             type="email"
             placeholder="Usuario@gmail.com"
-            name="email"
-            autoComplete="email"
-            required={true}
+            className="w-full mt-1 p-1 md:p-2 placeholder:text-sm lg:placeholder:text-base border border-gray-300 rounded-lg focus:outline-none focus:border-red-500 bg-gray-100"
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </div>
-        <div className="login-main-data">
-          <p>Contraseña</p>
+        <div className="mb-4">
+          <label className="block text-sm font-bold text-gray-700">Contraseña</label>
           <input
             type="password"
             placeholder="********"
-            name="password"
-            autoComplete="current-password"
-            required={true}
+            className="w-full mt-1 p-1 md:p-2 placeholder:text-sm lg:placeholder:text-base border border-gray-300 rounded-lg focus:outline-none focus:border-red-500 bg-gray-100"
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
         </div>
-        {error && <div className="login-main-error">{error}</div>}
-        <div className="login-main-forgotpassword">
+        {error && <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-2 text-sm font-semibold rounded-md">{error}</div>}
+        <div className="text-right text-sm text-red-500 hover:text-red-700">
           <Link href="/auth/password-recovery">¿Olvidaste la contraseña?</Link>
-          <p></p>
         </div>
-        <div className="login-main-button-container">
-          <button className="login-main-button">Iniciar Sesion</button>
+        <div className="flex justify-center mt-4">
+          {loading ? (
+            <button className="w-36 h-9 bg-red-700 text-white font-bold rounded-lg flex items-center justify-center">
+              <ImSpinner8 className="animate-spin w-5 h-5" />
+            </button>
+          ) : (
+            <button className="w-36 h-9 bg-red-700 hover:bg-red-900 text-white font-bold rounded-lg focus:outline-none">
+              Ingresar
+            </button>
+          )}
         </div>
       </form>
-      <div className="login-have-account">
-          <p>
-            No tenes una cuenta?{" "}
-            <Link href={"/auth/register"} className="login-have-account-redirect">Registrarse</Link>
-          </p>
-        </div>
+      <p className="mt-4 text-sm text-gray-600">
+        ¿No tenes una cuenta? {" "}
+        <Link href="/auth/register" className="text-red-500 hover:text-red-700 font-bold">
+          Registrarse
+        </Link>
+      </p>
     </div>
   );
 }
