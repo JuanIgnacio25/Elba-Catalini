095c7bd5c90f31d14eff86ee6c1f4f32c60f2282
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-04-03 17:54:11 -0300

    Modify, variable recovery token

--- src/utils/mail/sendVerificationMail.js
+++ src/utils/mail/sendVerificationMail.js
@@ -13,9 +13,9 @@ const sendVerificationMail = async (userEmail, verificationToken) => {
         <h2 style="color: #333; text-align: center;">Verificacion de Cuenta</h2>
         <p style="color: #555;">Gracias por registrarte. Para verificar tu cuenta, haz clic en el botón de abajo:</p>
         <div style="text-align: center; margin: 20px 0;">
-          <a href="${recoveryLink}" style="display: inline-block; padding: 10px 20px; font-size: 16px; color: #fff; background-color: #ef4444; text-decoration: none; border-radius: 5px; transition: background-color 0.3s ease">Verificar Cuenta</a>
+          <a href="${verificationToken}" style="display: inline-block; padding: 10px 20px; font-size: 16px; color: #fff; background-color: #ef4444; text-decoration: none; border-radius: 5px; transition: background-color 0.3s ease">Verificar Cuenta</a>
         </div>
-        <p style="color: #555;">Si no creaste esta cuenta, puedes ignorar este mensaje.</p>
+        <p style="width: 100%; text-align:center; color: #555;">Si no creaste esta cuenta, puedes ignorar este mensaje.</p>
         <p style="color: #999; font-size: 12px; text-align: center;">Este enlace expirará en 1 hora.</p>
       </div>
       <style>
