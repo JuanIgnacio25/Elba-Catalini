df85c601c955eeb4efae34123a3f2e0547285567
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-04-06 18:03:17 -0300

    Add, service & dao functions to merge localStorage cart & session cart

--- src/models/cart/CartService.js
+++ src/models/cart/CartService.js
@@ -1,5 +1,6 @@
 import CartDao from "./CartDao";
 import toNumericId from "@/utils/toNumericId";
+import validateQuantity from "@/utils/validate/validateQuantity";
 
 class CartService {
   constructor() {
@@ -15,7 +16,7 @@ class CartService {
     }
   }
 
-  async getCartById(cartId){
+  async getCartById(cartId) {
     try {
       const cart = await this.dao.getCartById(toNumericId(cartId));
       return cart;
@@ -24,7 +25,7 @@ class CartService {
     }
   }
 
-  getProductFromCartById(cart,productId) {
+  getProductFromCartById(cart, productId) {
     try {
       const product = cart.products.filter((e) => e.productId == productId);
       return product;
@@ -33,53 +34,60 @@ class CartService {
     }
   }
 
-  async addProductToCart(cartId,prod){
+  async addProductToCart(cartId, prod) {
     try {
-
       const cart = await this.getCartById(cartId);
-      if(!cart) throw new Error("El carrito no existe");
+      if (!cart) throw new Error("El carrito no existe");
 
-      const existingProduct = this.getProductFromCartById(cart,prod.productId);
+      const existingProduct = this.getProductFromCartById(cart, prod.productId);
 
-      if(existingProduct.length !== 0) {
-        
-        const newQuantity = Number(existingProduct[0].quantity) + Number(prod.quantity);
-        await this.dao.changeProductQuantityFromCart(cartId,prod.productId,newQuantity);
+      if (existingProduct.length !== 0) {
+        const newQuantity =
+          Number(existingProduct[0].quantity) + Number(prod.quantity);
+        await this.dao.changeProductQuantityFromCart(
+          cartId,
+          prod.productId,
+          newQuantity
+        );
 
-        return {name:prod.name, quantity:prod.quantity};
+        return { name: prod.name, quantity: prod.quantity };
       }
 
-      const updatedCart= await this.dao.addProductToCart(cartId,prod);
-      const {name , quantity} = updatedCart.products[updatedCart.products.length - 1];
+      const updatedCart = await this.dao.addProductToCart(cartId, prod);
+      const { name, quantity } =
+        updatedCart.products[updatedCart.products.length - 1];
 
-      return {name , quantity};
-
+      return { name, quantity };
     } catch (error) {
       throw error;
     }
   }
 
-  async addProductsArrayToCart(cartId,products){
+  async addProductsArrayToCart(cartId, products) {
     try {
-      const addedProducts = this.dao.addProductsArrayToCart(toNumericId(cartId),products);
+      const addedProducts = this.dao.addProductsArrayToCart(
+        toNumericId(cartId),
+        products
+      );
       return addedProducts;
     } catch (error) {
       throw error;
     }
   }
 
-  async removeProductFromCart(cartId,productId){
+  async removeProductFromCart(cartId, productId) {
     try {
       const cart = await this.getCartById(cartId);
-      const isInTheCart = this.getProductFromCartById(cart,productId);
-      if(isInTheCart.length == 0) throw new Error("El producto no se encontraba en el carrito");
-      await this.dao.removeProductFromCart(cartId,Number(productId));
+      const isInTheCart = this.getProductFromCartById(cart, productId);
+      if (isInTheCart.length == 0)
+        throw new Error("El producto no se encontraba en el carrito");
+      await this.dao.removeProductFromCart(cartId, Number(productId));
     } catch (error) {
       throw error;
     }
   }
 
-  async clearCart(cartId){
+  async clearCart(cartId) {
     try {
       const clearedCart = await this.dao.clearCart(toNumericId(cartId));
       return clearedCart;
@@ -90,8 +98,94 @@ class CartService {
 
   async removeProductFromAllCarts(productId) {
     try {
-      const clearedCart = await this.dao.removeProductFromAllCarts(toNumericId(productId));
+      const clearedCart = await this.dao.removeProductFromAllCarts(
+        toNumericId(productId)
+      );
       return clearedCart.modifiedCount;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async validateProductsQuantity(products) {
+    try {
+      const validatedProducts = products.map((prod) => {
+        if (!validateQuantity(prod.quantity)) {
+          return { productId: prod.productId, quantity: 1 };
+        }
+        return { productId: prod.productId, quantity: prod.quantity };
+      });
+
+      return validatedProducts;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async validateLocalProductsById(localProducts, allProducts) {
+    try {
+      const plainProducts = allProducts.map((product) => product.toObject());
+
+      const validatedProductsById = localProducts.map((prod) => {
+        const product = plainProducts.find(
+          (e) => e.productId === prod.productId
+        );
+        if (!product) return;
+        return { ...product, quantity: prod.quantity };
+      });
+
+      return validatedProductsById.filter((prod) => prod !== undefined);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async mergeProductsArray(localProducts, dbProducts, cartId) {
+    try {
+      const mergedCart = [...dbProducts];
+
+      localProducts.forEach((localProd) => {
+        const existingProduct = mergedCart.find(
+          (prod) => prod.productId === localProd.productId
+        );
+
+        if (existingProduct) {
+          existingProduct.quantity = Number(existingProduct.quantity) + Number(localProd.quantity);
+        } else {
+          mergedCart.push(localProd);
+        }
+      });
+
+      await this.dao.replaceProductsArray(cartId, mergedCart);
+
+      return mergedCart;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async mergeCarts(localProductsCart, cart, allProducts) {
+    try {
+      if (!cart) throw new Error("El carrito no existe");
+
+      if (localProductsCart.length === 0) return cart;
+
+      const validatedQuantityProducts = await this.validateProductsQuantity(
+        localProductsCart
+      );
+
+      const validatedProducts = await this.validateLocalProductsById(
+        validatedQuantityProducts,
+        allProducts
+      );
+
+      const mergedCart = await this.mergeProductsArray(
+        validatedProducts,
+        cart.products,
+        cart.cartId
+      );
+
+      return mergedCart;
     } catch (error) {
       throw error;
     }
