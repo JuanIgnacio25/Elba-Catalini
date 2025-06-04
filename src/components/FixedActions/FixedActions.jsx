0ecac94f51916bc8438ff24cab26e12956287915
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-06-04 16:49:53 -0300

    Delete: global top margin

--- src/components/FixedActions/FixedActions.jsx
+++ src/components/FixedActions/FixedActions.jsx
@@ -25,7 +25,7 @@ export default function FixedActions() {
   }, []);
 
   return (
-    <div className="fixed bottom-6 right-3 flex flex-col items-center gap-4 z-50">
+    <div className="fixed bottom-6 right-3 flex flex-col items-center gap-4 z-[1200]">
       <div className="w-9 md:w-11 h-9 md:h-11">
         {showScrollButton && (
           <button
