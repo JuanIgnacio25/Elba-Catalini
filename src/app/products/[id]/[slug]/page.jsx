1a510a348e924b54bd87e057b5dfac99b2385112
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-07-05 19:55:27 -0300

    Improve: metadata for each product

--- src/app/products/[id]/[slug]/page.jsx
+++ src/app/products/[id]/[slug]/page.jsx
@@ -1,5 +1,25 @@
 import ProductDetail from "@/components/views/Products/ProductDetail/ProductDetail";
 
+export async function generateMetadata({ params }) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_WEBSITE_DOMAIN}/api/products/${params.id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) return notFound();
+
+  const product = await res.json();
+  
+  return {
+    title: `${product.name}`,
+    description: product.description,
+    openGraph: {
+      title: product.name,
+      description: product.description,
+      images: [{ url: product.image }],
+    },
+  };
+}
+
 function productDetailPage({params}) {
   return (
     <ProductDetail id={params.id}/>
