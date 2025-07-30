8552c3f3f716a6dd7ad5cd27af5120906aaada87
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-07-30 14:13:36 -0300

    Refactor: saleImage endpoint to sale

---
+++ src/app/api/layoutImages/sale/route.js
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import { revalidatePath } from 'next/cache';
+import { connectDB } from "@/lib/mongodb";
+import SaleService from "@/models/sale/SaleService";
+import validateImage from "@/utils/validate/validateImage";
+import { uploadImageToCloudinary } from "@/utils/imageHandler/cloudinaryLayoutImagesHandler";
+
+const saleService = new SaleService();
+
+export async function GET() {
+  try {
+    await connectDB();
+    const sale = await saleService.getSale();
+    return NextResponse.json(sale, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+}
+
+export async function PUT(req) {
+  try {
+    const data = await req.formData();
+    const image = data.get("image");
+
+    if (validateImage([image]).length !== 0) {
+      return NextResponse.json(
+        { message: "No se subio imagen o exece los 3MB" },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const { secure_url, public_id } = await uploadImageToCloudinary(image, "sale_preset");
+    const saleImageToUpdate = {image: {secure_url , public_id}};
+    
+    await connectDB();
+
+    const uploadedSaleImage = await saleService.updateSale(saleImageToUpdate);
+    revalidatePath('/');
+
+    return NextResponse.json(uploadedSaleImage, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+}
