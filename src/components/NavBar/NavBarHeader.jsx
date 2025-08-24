af9c327c237fe1c76360c4eebf5e12b98d992909
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-08-24 13:40:49 -0300

    Improve: SEO adding /products url in nav button 'productos'

--- src/components/NavBar/NavBarHeader.jsx
+++ src/components/NavBar/NavBarHeader.jsx
@@ -6,7 +6,7 @@ import { FaFacebookF } from "react-icons/fa6";
 function NavBarHeader() {
   return (
     <div className="w-full h-10 bg-red-600">
-      <div className="container mx-auto h-full flex flex-row justify-end items-center text-white px-2 sm:px-0">
+      <div className="container lg:max-w-full mx-auto h-full flex flex-row justify-end items-center text-white px-2 sm:px-0 lg:px-4">
         <div className="flex flex-row items-center gap-1 sm:gap-2">
           <h3 className="font-semibold">Seguinos</h3>
           <Link
