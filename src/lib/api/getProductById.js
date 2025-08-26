a4e01861a17548f9a6bcdfe5950b7ef1ba60a258
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-08-26 16:22:20 -0300

    Improve: function using cache from react

--- src/lib/api/getProductById.js
+++ src/lib/api/getProductById.js
@@ -1,15 +1,15 @@
 import ProductService from "@/models/product/ProductService";
 import { connectDB } from "../mongodb";
+import { cache } from "react";
 
 const productService = new ProductService();
 
-export async function getProductById(id) {
+export const getProductById = cache(async (id) => {
   await connectDB();
-
   try {
     const product = await productService.findProductById(id);
     return JSON.parse(JSON.stringify(product));
   } catch (error) {
     throw error;
   }
-}
+});
