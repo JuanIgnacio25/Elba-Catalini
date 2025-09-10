4781bbd248548f00b10f283c560a374304846472
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-09-10 15:31:47 -0300

    Update: contact email

--- src/utils/mail/sendClientOrderMail.js
+++ src/utils/mail/sendClientOrderMail.js
@@ -77,7 +77,7 @@ async function sendClientOrderEmail(customerEmail, orderItems, orderId) {
           <h2>Gracias por elegirnos!</h2>
           <p>Tu pedido ha sido recibido y está siendo procesado.</p>
           <p>Nuestro equipo de compras se pondrá en contacto con usted para enviarle el presupuesto , y luego coordinar el pago.</p>
-          <p>Este mensaje se generó automáticamente. Si tiene alguna consulta, no dude en escribirnos a ventascatalini@gmail.com o al 3471 589042</p>
+          <p>Este mensaje se generó automáticamente. Si tiene alguna consulta, no dude en escribirnos a ventaselbabaiml@hotmail.com o al 3471 589042</p>
         </div>
         <div class="order-details">
           <h3>Detalles del Pedido</h3>
@@ -95,7 +95,6 @@ async function sendClientOrderEmail(customerEmail, orderItems, orderId) {
           </table>
         </div>
         <div class="footer">
-          <p>Si tenes alguna pregunta, contacta con nuestro servicio de atención al cliente.</p>
           <p>&copy; ${new Date().getFullYear()} Elba Catalini. Todos los derechos reservados.</p>
         </div>
       </div>
