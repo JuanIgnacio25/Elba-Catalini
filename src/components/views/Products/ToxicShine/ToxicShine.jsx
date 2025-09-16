84b52551783a44d27ec6f0aeee359f36871dc711
Juan Ignacio Colli <nachocolli1@gmail.com>
2025-09-16 16:20:10 -0300

    Improve: ToxicShine components to recive products from server

--- src/components/views/Products/ToxicShine/ToxicShine.jsx
+++ src/components/views/Products/ToxicShine/ToxicShine.jsx
@@ -2,29 +2,34 @@ import "@/components/views/Products/ToxicShine/toxicShine.css";
 
 import PathHeader from "@/components/common/PathHeader/PathHeader";
 import ToxicShineMain from "@/components/views/Products/ToxicShine/ToxicShineMain";
-import ProductsMainFallback from "@/components/Fallbacks/ProductsMainFallback/ProductsMainFallback";
 
-import { TOXIC_SHINE_CATEGORIES } from "@/constants/categories";
+function ToxicShine({
+  products,
+  currentPage,
+  totalPages,
+  totalProducts,
+  searchParams,
+  itemsPerPage,
+  initialCategories,
+}) {
 
-import { Suspense } from "react";
-
-function ToxicShine() {
   return (
     <div className="toxic-products-container">
       <PathHeader />
       <div className="w-full flex justify-center">
-        <h1 className="w-[90%] md:w-[80%] text-3xl text-red-500 font-bold">Toxic Shine</h1>
+        <h1 className="max-[871px]:w-[90%] w-[80%] text-3xl text-red-500 font-bold">
+          Toxic Shine
+        </h1>
       </div>
-      <Suspense
-        fallback={
-          <ProductsMainFallback
-            categories={TOXIC_SHINE_CATEGORIES}
-            enabled={false}
-          />
-        }
-      >
-        <ToxicShineMain />
-      </Suspense>
+      <ToxicShineMain
+        products={products}
+        currentPage={currentPage}
+        totalPages={totalPages}
+        totalProducts={totalProducts}
+        searchParams={searchParams}
+        itemsPerPage={itemsPerPage}
+        initialCategories={initialCategories}
+      />
     </div>
   );
 }
