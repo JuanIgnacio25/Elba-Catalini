d14e9c81729c5818b7f035233daf5e40f2b73ddf
Juan Ignacio Colli <nachocolli1@gmail.com>
2026-03-05 18:41:28 -0300

    Improve: hardcoding blur slides

--- src/components/views/home/Home.jsx
+++ src/components/views/home/Home.jsx
@@ -1,5 +1,3 @@
-import dynamic from "next/dynamic";
-
 import MainCarousel from "./MainCarousel/MainCarousel"
 import News from "./News/News";
 import Brands from "./Brands/Brands";
@@ -7,6 +5,8 @@ import Brands from "./Brands/Brands";
 const Services = dynamic(() => import("./Services"));
 const BenefitsIcons = dynamic(() => import("./BenefitsIcons"));
 const BusinessInfo = dynamic(() => import("./BusinessInfo/BusinessInfo"));
+
+export const revalidate = 86400;
 
 
 
--- src/lib/api/getSlidesDataWithBlur.js
+++ src/lib/api/getSlidesDataWithBlur.js
@@ -1,31 +1,14 @@
-import { getPlaiceholder } from "plaiceholder";
-
 export async function getSlidesDataWithBlur() {
-  const rawSlidesData = [
+  return [
     {
-      src: "https://res.cloudinary.com/dzvwrmykh/image/upload/f_auto,q_auto/v1750791980/autopista-noche_wpzddp.jpg",
-      overlayKey: "BaimlOverlay",
+      src: 'https://res.cloudinary.com/dzvwrmykh/image/upload/f_auto,q_auto/v1750791980/autopista-noche_wpzddp.jpg',
+      overlayKey: 'BaimlOverlay',
+      blurDataURL: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAMElEQVR4nGNgZ2CQlRDRUJZjYGRgaM2PenPtwPpEf1sVCYb/387N7q1xlhfzV5UAALwJC84KqejcAAAAAElFTkSuQmCC'
     },
     {
-      src: "https://res.cloudinary.com/dzvwrmykh/image/upload/f_auto,q_auto/v1750792143/toxic-shine-portada_zoycje.jpg",
-      overlayKey: "ToxicShineOverlay",
-    },
+      src: 'https://res.cloudinary.com/dzvwrmykh/image/upload/f_auto,q_auto/v1750792143/toxic-shine-portada_zoycje.jpg',
+      overlayKey: 'ToxicShineOverlay',
+      blurDataURL: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAACCAIAAADwyuo0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAI0lEQVR4nGPw8Q48dOjEl1//g8ITGMyVVBa01CxaNJ+BhQMApjsLNy8IEnoAAAAASUVORK5CYII='
+    }
   ];
-
-  const slidesWithBlur = await Promise.all(
-    rawSlidesData.map(async (slide) => {
-      try {
-        const response = await fetch(slide.src);
-        if (!response.ok) {
-          return { ...slide, blurDataURL: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" };
-        }
-        const buffer = Buffer.from(await response.arrayBuffer());
-        const { base64: generatedBase64 } = await getPlaiceholder(buffer);
-        return { ...slide, blurDataURL: generatedBase64 };
-      } catch (error) {
-        return { ...slide, blurDataURL: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" };
-      }
-    })
-  );
-  return slidesWithBlur;
 }
