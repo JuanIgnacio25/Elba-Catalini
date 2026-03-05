fd4e67fa69c442b6b91ecbb53f6e5188f5f86b16
Juan Ignacio Colli <nachocolli1@gmail.com>
2026-03-05 17:52:41 -0300

    Improve: Products Cards styles

--- src/components/common/AnimatedProductCard.jsx
+++ src/components/common/AnimatedProductCard.jsx
@@ -4,14 +4,17 @@ import { motion } from "framer-motion";
 
 function AnimatedProductCard({ prod, delay, ProductCard }) {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.3 }}
-      transition={{ duration: 0.5, delay: delay || 0.1, ease: "easeOut" }}
-    >
-      <ProductCard prod={prod} />
-    </motion.div>
+    <div>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.3 }}
+        transition={{ duration: 0.5, delay: delay || 0.1, ease: "easeOut" }}
+        style={{ willChange: "opacity, transform" }}
+      >
+        <ProductCard prod={prod} />
+      </motion.div>
+    </div>
   );
 }
 
