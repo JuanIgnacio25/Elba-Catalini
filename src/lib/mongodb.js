ed3a827dd7af92ef77757ab319800fb8ae53e880
Juan Ignacio Colli <nachocolli1@gmail.com>
2026-03-05 18:14:07 -0300

    Improve: mongoatlas connection

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,16 +1,34 @@
 "use server"
+import mongoose from "mongoose";
 
-import mongoose  from "mongoose";
+// Usamos el objeto global para que la conexión persista entre recargas del servidor
+let cached = global.mongoose;
 
-let connection = null;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 export const connectDB = async () => {
-  if (connection) return connection;
-  try {
-    connection = await mongoose.connect(process.env.MONGO_ATLAS_URL);
-    
-    return connection;
-  } catch (err) {
-    throw err;
+
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(process.env.MONGO_ATLAS_URL, opts).then((mongoose) => {
+      console.log("✅ Nueva conexión a MongoDB establecida");
+      return mongoose;
+    });
   }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 };
